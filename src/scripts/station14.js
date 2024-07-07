40968b8967234e77d0d6ada1585d0aa2f40839c2
tech-kinako <tech.kinako@gmail.com>
2024-07-07 12:27:29 +0900

    clear station14

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -1,3 +1,6 @@
+const { log } = require("fp-ts/lib/Console");
+const { first } = require("fp-ts/lib/Semigroup");
+
 function getData() {
     // これから作ろうとしているもので、JavaScriptでフルネームを生成する必要が出てきました。
     // ただ現状持っている情報では、名前と苗字をそれぞれのみしかありません。
@@ -10,9 +13,8 @@ function getData() {
 }
 
 function buildFullName(data) {
-    // Station14の問題はこちらです。想定する答えをここを書いてください。
-    // 氏名がわかるようにしつつ、半角スペースで繋いでください。
-    // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+    data.full_name = data.family_name +' '+ data.first_name;
+    return data;
 }
 
 // 出力が想定される答え
